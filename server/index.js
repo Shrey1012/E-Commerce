b78151be8f559f725b0d990452e8823e5f750192
Shrey1012 <shrey9157@gmail.com>
2022-07-26 23:19:14 +0530

    Added Payment route

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const StripeRoute = require("./routes/stripe");
+const cors = require("cors")
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ mongoose
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -26,6 +29,8 @@ app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 
 app.use("/api/orders", orderRoute);
+
+app.use("/api/checkout",StripeRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running on port 5000");
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -26,7 +26,10 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(401).json("Wrong credentials!");
+    if (!user) {
+      res.status(401).json("Wrong credentials!");
+      return;
+    }
 
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
@@ -34,8 +37,10 @@ router.post("/login", async (req, res) => {
     );
     const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-    originalPassword !== req.body.password &&
+    if (originalPassword !== req.body.password) {
       res.status(401).json("Wrong credentials!");
+      return;
+    }
 
     const accessToken = jwt.sign(
       {
